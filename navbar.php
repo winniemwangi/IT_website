c85d1f9594cb8573a5855918088cab35369bbc4a
winniemwangi <winniewameremwangi@gmail.com>
2022-06-27 17:37:24 +0300

    add it company website

---
+++ navbar.php
@@ -0,0 +1,10 @@
+<div class="navigate">
+     <nav>
+         <ul>
+             <li><a href="index.html">Home</a></li>
+             <li><a href="about.html">Who we are</a></li>
+             <li><a href="services.html">Our services</a></li>
+             <li><a href="contact.html">Contact us</a></li>
+         </ul>
+     </nav>
+</div>
